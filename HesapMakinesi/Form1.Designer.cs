6053477e0268b3b06c4ed080566150cf1f22947c
Umitsrsc <sarsici.a@gmail.com>
2023-06-18 10:33:48 +0300

    Add project files.

---
+++ HesapMakinesi/Form1.Designer.cs
@@ -0,0 +1,413 @@
+﻿
+namespace HesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btn_artieksi = new System.Windows.Forms.Button();
+            this.btn_gerial = new System.Windows.Forms.Button();
+            this.btn_CE = new System.Windows.Forms.Button();
+            this.btn_C = new System.Windows.Forms.Button();
+            this.btn_karekok = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.btn_bol = new System.Windows.Forms.Button();
+            this.btn_kare = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.btn_carp = new System.Windows.Forms.Button();
+            this.btn_birbolu = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button23 = new System.Windows.Forms.Button();
+            this.btn_cikar = new System.Windows.Forms.Button();
+            this.button26 = new System.Windows.Forms.Button();
+            this.btn_ondalikvirgul = new System.Windows.Forms.Button();
+            this.btn_topla = new System.Windows.Forms.Button();
+            this.btn_esittir = new System.Windows.Forms.Button();
+            this.txt_sayi_giris = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "MC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.hafıza_islem);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(116, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "MR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.hafıza_islem);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(172, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "M+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.hafıza_islem);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(228, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 32);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "M-";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.hafıza_islem);
+            // 
+            // btn_artieksi
+            // 
+            this.btn_artieksi.Location = new System.Drawing.Point(284, 64);
+            this.btn_artieksi.Name = "btn_artieksi";
+            this.btn_artieksi.Size = new System.Drawing.Size(50, 32);
+            this.btn_artieksi.TabIndex = 4;
+            this.btn_artieksi.Text = "+-";
+            this.btn_artieksi.UseVisualStyleBackColor = true;
+            this.btn_artieksi.Click += new System.EventHandler(this.btn_artieksi_Click);
+            // 
+            // btn_gerial
+            // 
+            this.btn_gerial.Location = new System.Drawing.Point(60, 102);
+            this.btn_gerial.Name = "btn_gerial";
+            this.btn_gerial.Size = new System.Drawing.Size(106, 32);
+            this.btn_gerial.TabIndex = 5;
+            this.btn_gerial.Text = "<-";
+            this.btn_gerial.UseVisualStyleBackColor = true;
+            this.btn_gerial.Click += new System.EventHandler(this.btn_gerial_Click);
+            // 
+            // btn_CE
+            // 
+            this.btn_CE.Location = new System.Drawing.Point(172, 102);
+            this.btn_CE.Name = "btn_CE";
+            this.btn_CE.Size = new System.Drawing.Size(50, 32);
+            this.btn_CE.TabIndex = 7;
+            this.btn_CE.Text = "CE";
+            this.btn_CE.UseVisualStyleBackColor = true;
+            this.btn_CE.Click += new System.EventHandler(this.btn_CE_Click);
+            // 
+            // btn_C
+            // 
+            this.btn_C.Location = new System.Drawing.Point(228, 102);
+            this.btn_C.Name = "btn_C";
+            this.btn_C.Size = new System.Drawing.Size(50, 32);
+            this.btn_C.TabIndex = 8;
+            this.btn_C.Text = "C";
+            this.btn_C.UseVisualStyleBackColor = true;
+            this.btn_C.Click += new System.EventHandler(this.btn_C_Click);
+            // 
+            // btn_karekok
+            // 
+            this.btn_karekok.Location = new System.Drawing.Point(284, 102);
+            this.btn_karekok.Name = "btn_karekok";
+            this.btn_karekok.Size = new System.Drawing.Size(50, 32);
+            this.btn_karekok.TabIndex = 9;
+            this.btn_karekok.Text = "√";
+            this.btn_karekok.UseVisualStyleBackColor = true;
+            this.btn_karekok.Click += new System.EventHandler(this.diger_islemler);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(60, 140);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(50, 32);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "7";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.rakam_girisleri);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(116, 140);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(50, 32);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "8";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.rakam_girisleri);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(172, 140);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(50, 32);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "9";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.rakam_girisleri);
+            // 
+            // btn_bol
+            // 
+            this.btn_bol.Location = new System.Drawing.Point(228, 140);
+            this.btn_bol.Name = "btn_bol";
+            this.btn_bol.Size = new System.Drawing.Size(50, 32);
+            this.btn_bol.TabIndex = 13;
+            this.btn_bol.Text = "/";
+            this.btn_bol.UseVisualStyleBackColor = true;
+            this.btn_bol.Click += new System.EventHandler(this.dort_islem);
+            // 
+            // btn_kare
+            // 
+            this.btn_kare.Location = new System.Drawing.Point(284, 140);
+            this.btn_kare.Name = "btn_kare";
+            this.btn_kare.Size = new System.Drawing.Size(50, 32);
+            this.btn_kare.TabIndex = 14;
+            this.btn_kare.Text = "X2";
+            this.btn_kare.UseVisualStyleBackColor = true;
+            this.btn_kare.Click += new System.EventHandler(this.diger_islemler);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(60, 178);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(50, 32);
+            this.button16.TabIndex = 15;
+            this.button16.Text = "4";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.rakam_girisleri);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(116, 178);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(50, 32);
+            this.button17.TabIndex = 16;
+            this.button17.Text = "5";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.rakam_girisleri);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(172, 178);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(50, 32);
+            this.button18.TabIndex = 17;
+            this.button18.Text = "6";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.rakam_girisleri);
+            // 
+            // btn_carp
+            // 
+            this.btn_carp.Location = new System.Drawing.Point(228, 178);
+            this.btn_carp.Name = "btn_carp";
+            this.btn_carp.Size = new System.Drawing.Size(50, 32);
+            this.btn_carp.TabIndex = 18;
+            this.btn_carp.Text = "x";
+            this.btn_carp.UseVisualStyleBackColor = true;
+            this.btn_carp.Click += new System.EventHandler(this.dort_islem);
+            // 
+            // btn_birbolu
+            // 
+            this.btn_birbolu.Location = new System.Drawing.Point(284, 178);
+            this.btn_birbolu.Name = "btn_birbolu";
+            this.btn_birbolu.Size = new System.Drawing.Size(50, 32);
+            this.btn_birbolu.TabIndex = 19;
+            this.btn_birbolu.Text = "1/x";
+            this.btn_birbolu.UseVisualStyleBackColor = true;
+            this.btn_birbolu.Click += new System.EventHandler(this.diger_islemler);
+            // 
+            // button21
+            // 
+            this.button21.Location = new System.Drawing.Point(60, 216);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(50, 32);
+            this.button21.TabIndex = 20;
+            this.button21.Text = "1";
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.rakam_girisleri);
+            // 
+            // button22
+            // 
+            this.button22.Location = new System.Drawing.Point(116, 216);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(50, 32);
+            this.button22.TabIndex = 21;
+            this.button22.Text = "2";
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.rakam_girisleri);
+            // 
+            // button23
+            // 
+            this.button23.Location = new System.Drawing.Point(172, 216);
+            this.button23.Name = "button23";
+            this.button23.Size = new System.Drawing.Size(50, 32);
+            this.button23.TabIndex = 22;
+            this.button23.Text = "3";
+            this.button23.UseVisualStyleBackColor = true;
+            this.button23.Click += new System.EventHandler(this.rakam_girisleri);
+            // 
+            // btn_cikar
+            // 
+            this.btn_cikar.Location = new System.Drawing.Point(228, 216);
+            this.btn_cikar.Name = "btn_cikar";
+            this.btn_cikar.Size = new System.Drawing.Size(50, 32);
+            this.btn_cikar.TabIndex = 23;
+            this.btn_cikar.Text = "-";
+            this.btn_cikar.UseVisualStyleBackColor = true;
+            this.btn_cikar.Click += new System.EventHandler(this.dort_islem);
+            // 
+            // button26
+            // 
+            this.button26.Location = new System.Drawing.Point(60, 254);
+            this.button26.Name = "button26";
+            this.button26.Size = new System.Drawing.Size(106, 32);
+            this.button26.TabIndex = 25;
+            this.button26.Text = "0";
+            this.button26.UseVisualStyleBackColor = true;
+            this.button26.Click += new System.EventHandler(this.rakam_girisleri);
+            // 
+            // btn_ondalikvirgul
+            // 
+            this.btn_ondalikvirgul.Location = new System.Drawing.Point(172, 254);
+            this.btn_ondalikvirgul.Name = "btn_ondalikvirgul";
+            this.btn_ondalikvirgul.Size = new System.Drawing.Size(50, 32);
+            this.btn_ondalikvirgul.TabIndex = 27;
+            this.btn_ondalikvirgul.Text = ".";
+            this.btn_ondalikvirgul.UseVisualStyleBackColor = true;
+            this.btn_ondalikvirgul.Click += new System.EventHandler(this.btn_ondalikvirgul_Click);
+            // 
+            // btn_topla
+            // 
+            this.btn_topla.Location = new System.Drawing.Point(228, 254);
+            this.btn_topla.Name = "btn_topla";
+            this.btn_topla.Size = new System.Drawing.Size(50, 32);
+            this.btn_topla.TabIndex = 28;
+            this.btn_topla.Text = "+";
+            this.btn_topla.UseVisualStyleBackColor = true;
+            this.btn_topla.Click += new System.EventHandler(this.dort_islem);
+            // 
+            // btn_esittir
+            // 
+            this.btn_esittir.Location = new System.Drawing.Point(284, 216);
+            this.btn_esittir.Name = "btn_esittir";
+            this.btn_esittir.Size = new System.Drawing.Size(50, 70);
+            this.btn_esittir.TabIndex = 29;
+            this.btn_esittir.Text = "=";
+            this.btn_esittir.UseVisualStyleBackColor = true;
+            this.btn_esittir.Click += new System.EventHandler(this.btn_esittir_Click);
+            // 
+            // txt_sayi_giris
+            // 
+            this.txt_sayi_giris.Location = new System.Drawing.Point(60, 13);
+            this.txt_sayi_giris.Multiline = true;
+            this.txt_sayi_giris.Name = "txt_sayi_giris";
+            this.txt_sayi_giris.Size = new System.Drawing.Size(274, 37);
+            this.txt_sayi_giris.TabIndex = 30;
+            this.txt_sayi_giris.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_sayi_giris_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 339);
+            this.Controls.Add(this.txt_sayi_giris);
+            this.Controls.Add(this.btn_esittir);
+            this.Controls.Add(this.btn_topla);
+            this.Controls.Add(this.btn_ondalikvirgul);
+            this.Controls.Add(this.button26);
+            this.Controls.Add(this.btn_cikar);
+            this.Controls.Add(this.button23);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.btn_birbolu);
+            this.Controls.Add(this.btn_carp);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.btn_kare);
+            this.Controls.Add(this.btn_bol);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.btn_karekok);
+            this.Controls.Add(this.btn_C);
+            this.Controls.Add(this.btn_CE);
+            this.Controls.Add(this.btn_gerial);
+            this.Controls.Add(this.btn_artieksi);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btn_artieksi;
+        private System.Windows.Forms.Button btn_gerial;
+        private System.Windows.Forms.Button btn_CE;
+        private System.Windows.Forms.Button btn_C;
+        private System.Windows.Forms.Button btn_karekok;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button btn_bol;
+        private System.Windows.Forms.Button btn_kare;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button btn_carp;
+        private System.Windows.Forms.Button btn_birbolu;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button btn_cikar;
+        private System.Windows.Forms.Button button26;
+        private System.Windows.Forms.Button btn_ondalikvirgul;
+        private System.Windows.Forms.Button btn_topla;
+        private System.Windows.Forms.Button btn_esittir;
+        private System.Windows.Forms.TextBox txt_sayi_giris;
+    }
+}
+
---
+++ HesapMakinesi/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        decimal sayi1, sayi2, hafıza_deger;
+        Boolean sonuclandi;
+        string islem;
+
+        
+
+        private void txt_sayi_giris_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) == false)
+            {
+                e.Handled = true;
+            }
+        }
+
+        
+
+        private void rakam_girisleri(object sender, EventArgs e)
+        {
+            if (sonuclandi)
+            {
+                txt_sayi_giris.Text = (sender as Button).Text;
+                sonuclandi = false;
+            }
+            else
+            {
+                txt_sayi_giris.Text = txt_sayi_giris.Text + (sender as Button).Text;
+            }
+        }
+
+       
+
+        private void btn_artieksi_Click(object sender, EventArgs e)
+        {
+            if (txt_sayi_giris.Text.Length>0)
+            {
+                if (txt_sayi_giris.Text.Substring(0,1)=="-")
+                {
+                    txt_sayi_giris.Text = txt_sayi_giris.Text.Substring(1);
+                }
+                else
+                {
+                    txt_sayi_giris.Text = "-"+txt_sayi_giris.Text.Substring(0);
+                }
+            }
+        }
+
+        
+
+        private void btn_ondalikvirgul_Click(object sender, EventArgs e)
+        {
+            if (txt_sayi_giris.Text.IndexOf(",")<=0)
+            {
+                if (txt_sayi_giris.Text.Length == 0)
+                {
+                    txt_sayi_giris.Text = "0";
+                }
+                else
+                {
+                    txt_sayi_giris.Text = txt_sayi_giris.Text + ",";
+                }
+            }
+        }
+
+        
+
+        private void btn_CE_Click(object sender, EventArgs e)
+        {
+            hafıza_deger = 0;
+        }
+
+        
+
+        private void btn_C_Click(object sender, EventArgs e)
+        {
+            txt_sayi_giris.Text = "";
+        }
+
+        
+
+        private void btn_gerial_Click(object sender, EventArgs e)
+        {
+            if (txt_sayi_giris.Text.Length>0)
+            {
+                txt_sayi_giris.Text = txt_sayi_giris.Text.Substring(0, txt_sayi_giris.Text.Length - 1);
+            }
+        }
+
+        
+
+        private void hafıza_islem(object sender, EventArgs e)
+        {
+            if (txt_sayi_giris.Text.Length == 0)
+            {
+                txt_sayi_giris.Text = "0";
+            }
+            if ((sender as Button).Name=="btn_MC")
+            {
+                hafıza_deger = 0;
+                sonuclandi = true;
+            }
+            if ((sender as Button).Name=="btn_MR")
+            {
+                txt_sayi_giris.Text = hafıza_deger.ToString();
+                sonuclandi = true;
+            }
+            if ((sender as Button).Name == "btn_Marti")
+            {
+                hafıza_deger=hafıza_deger  + Convert.ToDecimal(txt_sayi_giris.Text);
+                sonuclandi = true;
+            }
+            if ((sender as Button).Name == "btn_Meksi")
+            {
+                hafıza_deger = hafıza_deger - Convert.ToDecimal(txt_sayi_giris.Text);
+                sonuclandi = true;
+            }
+        }
+
+        
+
+        private void dort_islem(object sender, EventArgs e)
+        {
+            islem = (sender as Button).Name;
+            if (txt_sayi_giris.Text.Length==0)
+            {
+                txt_sayi_giris.Text = "0";
+            }
+            sayi1 = Convert.ToDecimal(txt_sayi_giris.Text);
+            sonuclandi = true;
+        }
+        private void btn_esittir_Click(object sender, EventArgs e)
+        {
+            sayi2 = Convert.ToDecimal(txt_sayi_giris.Text);
+            decimal islem_sonucu = 0;
+            if (islem=="btn_topla")
+            {
+                islem_sonucu = sayi1 + sayi2;
+            }
+            if (islem == "btn_cikar")
+            {
+                islem_sonucu = sayi1 - sayi2;
+            }
+            if (islem == "btn_carp")
+            {
+                islem_sonucu = sayi1 * sayi2;
+            }
+            if (islem == "btn_bol")
+            {
+                islem_sonucu = sayi1 / sayi2;
+            }
+            sonuclandi = true;
+            txt_sayi_giris.Text = islem_sonucu.ToString();
+        }
+        private void diger_islemler(object sender, EventArgs e)
+        {
+            if (txt_sayi_giris.Text.Length==0)
+            {
+                txt_sayi_giris.Text = "0";
+            }
+            decimal diger_islemler_sayi = Convert.ToDecimal(txt_sayi_giris.Text);
+            decimal sonuc = 0;
+            if (((sender as Button).Name=="btn_karekok")&&diger_islemler_sayi>0)
+            {
+                sonuc = Convert.ToDecimal(Math.Sqrt((double)diger_islemler_sayi));
+            }
+            if ((sender as Button).Name=="btn_kare")
+            {
+                sonuc = diger_islemler_sayi * diger_islemler_sayi;
+            }
+            if ((sender as Button).Name == "btn_birbolu")
+            {
+                sonuc = 1 * diger_islemler_sayi;
+            }
+            txt_sayi_giris.Text = sonuc.ToString();
+            sonuclandi = true;
+        }
+
+
+
+
+
+
+    }
+}
